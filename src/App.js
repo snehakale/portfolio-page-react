675c49a299027e6a64e8203b8a16c85903620c6f
Sneha Kale <kalesneha88@gmail.com>
2021-10-21 10:06:25 -0400

    Add new technologies component and js, style files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Summary from './components/summary/summary';
+import Technologies from './components/technologies/technologies';
 
 function App() {
   return (
     <div className="App">
       <Summary />
+      <Technologies />
     </div>
   );
 }
---
+++ src/components/technologies/technologies.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './technologies.css'
+
+function Technologies(props) {
+    return (
+        <div className="technologies-container">
+            List of Technologies 
+        </div>
+    );
+}
+
+export default Technologies;
