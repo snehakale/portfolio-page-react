f5307bde3e30655edaf58e7e57feca9db0dbfa23
Sneha Kale <kalesneha88@gmail.com>
2021-10-20 23:06:36 -0400

    Add new summary component and js and style files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Summary from './components/summary/summary';
 
 function App() {
   return (
     <div className="App">
-      Initial setup!!
+      <Summary />
     </div>
   );
 }
---
+++ src/components/summary/summary.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './summary.css';
+
+function Summary(props) {
+    return (
+        <div className="summary-container">
+            Summary Data
+        </div>
+    );
+}
+
+export default Summary;
