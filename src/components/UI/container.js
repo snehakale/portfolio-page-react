96ff4c0aeb3fba674baf1da322f16a6ba9db128f
Sneha Kale <kalesneha88@gmail.com>
2021-10-22 10:52:47 -0400

    Add new component Personal data and js, style files

--- src/components/UI/container.js
+++ src/components/UI/container.js
@@ -1,11 +1,13 @@
 import Summary from '../summary/summary';
 import Technologies from '../technologies/technologies';
+import PersonalData from '../personal/personal';
 import './container.css';
 
 function Container() {
     return (
         <div className="container-style">
-            <div className="container-left">
+            <div className="container-left"> 
+                <PersonalData />
             </div>
             <div className="container-right">
                 <Summary />
---
+++ src/components/personal/personal.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './personal.css'
+
+function PersonalData(props) {
+    return (
+        <div className="personal-data-container">
+            Personal Data 
+        </div>
+    );
+}
+
+export default PersonalData;
