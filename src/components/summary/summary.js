66bbaa6a1d08d66b0e938beaf3eb96ae8038db15
Sneha Kale <kalesneha88@gmail.com>
2021-10-28 11:10:15 -0400

    Update Summary component and it's style classes

--- src/components/summary/summary.js
+++ src/components/summary/summary.js
@@ -4,7 +4,13 @@ import './Summary.css';
 function Summary(props) {
     return (
         <div className="summary-container">
-            Summary Data
+            <p className="summary-data"> 
+                An ambitious UI developer with proven expeience in UI, Front end development technolgies such as HTML,CSS, JavaScript, 
+                Angular, React.js, Knockout.js, programing languages such as Python, Java, database technologies such as MongoDB, MySQL,
+                PostgreSQL. Participated in all stages of software development life cycle. A forward-thinking person with excellent 
+                communication, analytical and time management skills; exceptionally well organized, self-motivated, team oriented, 
+                who enjoys being challenged in work.
+            </p>
         </div>
     );
 }
