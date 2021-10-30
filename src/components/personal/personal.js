b9a9342aa284dcf069b8964bc094f0100cc8d0d7
Sneha Kale <kalesneha88@gmail.com>
2021-10-29 22:49:57 -0400

    Update Personal data component

--- src/components/personal/personal.js
+++ src/components/personal/personal.js
@@ -4,7 +4,15 @@ import './Personal.css'
 function PersonalData(props) {
     return (
         <div className="personal-data-container">
-            Personal Data 
+            <div className="personal-name">
+                Sneha Kale
+            </div>
+            <div>
+                Atlanta, GA
+            </div>
+            <div>
+                Front End / UI Developer
+            </div>
         </div>
     );
 }
