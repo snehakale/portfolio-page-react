41ab7b7df6f38e4ceb1f17cb8554a60b2e8b2319
Sneha Kale <kalesneha88@gmail.com>
2021-11-03 09:50:05 -0400

    Update Academics component

--- src/components/academics/Academic.js
+++ src/components/academics/Academic.js
@@ -3,8 +3,13 @@ import './Academic.css';
 
 function Academics(props) {
     return (
-        <div className="academics-containerr">
-            Academics
+        <div className="academics-container">
+            <div className="academics-title">
+                Academics
+            </div>
+            <div className="academics-descr">
+                Bachelor's in Engineering in Information Technology
+            </div>
         </div>
     );
 }
